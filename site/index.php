23bd3920edb82e3359aac24c1fabc754e2c7193d
Torshken <pederhatlen@gmail.com>
2022-03-08 00:10:28 +0100

    Further tweaks

--- site/index.php
+++ site/index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>C.R.U.D.</title>
+    <title>C.R.U.D</title>
     <link rel="stylesheet" href="css/style.css">
 
     <!-- Icons from material icons by Google -->
@@ -29,17 +29,20 @@
             $con->close();
 
             for ($i=0; $i < count($result); $i++) { 
-                echo '<div class="message" data-msgid="'.$result[$i]["id"].'" data-uuid="'.$result[$i]["user"].'"><p><span class="time">['.strtotime($result[$i]["sent_at"]).']</span>&nbsp;'.$result[$i]["message"].'</p>'.
-                ($result[$i]["user"] == $_SESSION["uuid"]? "<span class='options'><a name='edit' href='#' onclick='showEdit(\"".$result[$i]["id"]."\", \"".$result[$i]["user"]."\")'><span class='material-icons editBTN'>edit</span></a><a name='delete' href='#' onclick='deleteMSG(\"".$result[$i]["id"]."\", \"".$result[$i]["user"]."\");'><span class='material-icons deleteBTN'>delete</span></a></span>":'').
-                '</div>';
+                echo '<div class="message" data-msgid="'.$result[$i]["id"].'" data-uuid="'.$result[$i]["user"].'"><p><span class="time">['.date("d.m H:i", strtotime($result[$i]["sent_at"])).']</span>&nbsp;'.$result[$i]["message"].'</p>'.
+                ($result[$i]["user"] == $_SESSION["uuid"]? "<span class='options'><a name='edit' href='#' onclick='showEdit(\"".$result[$i]["id"]."\", \"".$result[$i]["user"]."\")'><span class='material-icons editBTN'>edit</span></a><a name='delete' href='#' onclick='deleteMSG(\"".$result[$i]["id"]."\", \"".$result[$i]["user"]."\");'><span class='material-icons deleteBTN'>delete</span></a></span>":'').'</div>';
             }
         ?>
     </div>
-    <form id="sendMSG" action="php/post.php" method="post">
-        <input type="hidden" name="form" value="send">
-        <input type="text" name="message" id="sendMessage" placeholder="Message (max 255 char)" maxlength="255">
-        <input type="submit" value="Send">
-    </form>
+    <footer>
+        <form id="sendMSG" action="php/post.php" method="post">
+            <input type="hidden" name="form" value="send">
+            <input type="text" name="message" id="sendMessage" placeholder="Message (max 255 char)" maxlength="255">
+            <input type="submit" value="Send">
+        </form>
+        <span>Peder 2022</span>
+    </footer>
+
     <div id="editFormWrapper">
         <form id="editMSG" action="php/post.php" method="post">
             <h2>Edit <a class="deleteBTN" href="#" onclick="editFormWrapperEl.style.display = '';">X</a></h2>
@@ -50,6 +53,7 @@
             <input type="submit" value="Send">
         </form>
     </div>
+    
     <form id="deleteMSG" action="php/post.php" method="post">
         <input type="hidden" name="form" value="delete">
         <input type="hidden" name="elementid" id="deleteElementId">
